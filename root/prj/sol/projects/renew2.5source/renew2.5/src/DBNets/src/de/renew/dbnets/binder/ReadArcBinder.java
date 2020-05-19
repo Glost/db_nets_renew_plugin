5bda85befa8adae0e6348fa2065bcd9fbf64f158
Anton Rigin <anton19979@yandex.ru>
2020-05-19 22:03:45 +0300

    #4 read arc simulation fixed

--- root/prj/sol/projects/renew2.5source/renew2.5/src/DBNets/src/de/renew/dbnets/binder/ReadArcBinder.java
+++ root/prj/sol/projects/renew2.5source/renew2.5/src/DBNets/src/de/renew/dbnets/binder/ReadArcBinder.java
@@ -4,15 +4,28 @@ import de.renew.engine.searcher.BindingBadness;
 import de.renew.engine.searcher.Searcher;
 import de.renew.net.PlaceInstance;
 import de.renew.net.TokenReserver;
+import de.renew.net.ViewPlaceInstance;
 import de.renew.net.arc.InputArcBinder;
+import de.renew.unify.Impossible;
+import de.renew.unify.StateRecorder;
+import de.renew.unify.Unify;
 import de.renew.unify.Variable;
 
 public class ReadArcBinder extends InputArcBinder {
 
+    private final Variable tokenVariable;
+
+    private final StateRecorder stateRecorder;
+
     private boolean isBound = false;
 
-    public ReadArcBinder(Variable variable, Variable delayVar, PlaceInstance placeInstance) {
-        super(variable, delayVar, placeInstance);
+    public ReadArcBinder(Variable tokenVariable,
+                         Variable delayVariable,
+                         PlaceInstance placeInstance,
+                         StateRecorder stateRecorder) {
+        super(tokenVariable, delayVariable, placeInstance);
+        this.tokenVariable = tokenVariable;
+        this.stateRecorder = stateRecorder;
     }
 
     @Override
@@ -37,16 +50,22 @@ public class ReadArcBinder extends InputArcBinder {
 
     @Override
     public void bind(Searcher searcher) {
-//        ViewPlaceInstance placeInstance = (ViewPlaceInstance) getPlaceInstance();
-//
-//        searcher.insertTriggerable(placeInstance.triggerables());
-
-        // TODO: implement.
-
-        if (!isBound) {
-            isBound = true;
-
-            searcher.search();
+        if (isBound) {
+            return;
         }
+
+        ViewPlaceInstance placeInstance = (ViewPlaceInstance) getPlaceInstance();
+
+        Object queryResult = placeInstance.executeQuery();
+
+        try {
+            Unify.unify(tokenVariable, queryResult, stateRecorder);
+        } catch (Impossible e) {
+            throw new RuntimeException(); // TODO: ...
+        }
+
+        isBound = true;
+
+        searcher.search();
     }
 }
--- root/prj/sol/projects/renew2.5source/renew2.5/src/DBNets/src/de/renew/net/arc/ReadArcOccurence.java
+++ root/prj/sol/projects/renew2.5source/renew2.5/src/DBNets/src/de/renew/net/arc/ReadArcOccurence.java
@@ -1,16 +1,13 @@
 package de.renew.net.arc;
 
 import de.renew.dbnets.binder.ReadArcBinder;
-import de.renew.dbnets.executable.ReadArcQueryExecutable;
 import de.renew.engine.searcher.Binder;
 import de.renew.engine.searcher.Executable;
 import de.renew.engine.searcher.Searcher;
 import de.renew.engine.searcher.VariableMapperCopier;
 import de.renew.expression.VariableMapper;
 import de.renew.net.DBNetControlLayerInstance;
-import de.renew.net.ViewPlaceInstance;
 import de.renew.unify.Impossible;
-import de.renew.unify.StateRecorder;
 import de.renew.unify.UnifyUtils;
 import de.renew.unify.Variable;
 
@@ -21,8 +18,6 @@ import java.util.Objects;
 public class ReadArcOccurence extends ArcOccurrence {
 
     private ReadArcBinder binder;
-
-    private StateRecorder stateRecorder;
 
     public ReadArcOccurence(ReadArc arc, VariableMapper mapper, DBNetControlLayerInstance netInstance) {
         super(arc, mapper, netInstance);
@@ -35,28 +30,23 @@ public class ReadArcOccurence extends ArcOccurrence {
             return Collections.singleton(binder);
         }
 
-        Object evaluated = arc.tokenExpr.startEvaluation(mapper,
+        Object evaluated = arc.tokenExpr.startEvaluation(
+                mapper,
                 searcher.recorder,
-                searcher.calcChecker);
+                searcher.calcChecker
+        );
 
-        if (UnifyUtils.isInstanceOfUnknown(evaluated)) {
-            tokenVar = new Variable(evaluated, searcher.recorder);
-        } else {
-            tokenVar = new Variable();
-        }
+        tokenVar = UnifyUtils.isInstanceOfUnknown(evaluated) ?
+                new Variable(evaluated, searcher.recorder) :
+                new Variable();
 
-        stateRecorder = searcher.recorder;
-
-        binder = new ReadArcBinder(tokenVar, delayVar, placeInstance);
+        binder = new ReadArcBinder(tokenVar, delayVar, placeInstance, searcher.recorder);
 
         return Collections.singleton(binder);
     }
 
-    // TODO: check downcast.
     @Override
     public Collection<Executable> makeExecutables(VariableMapperCopier variableMapperCopier) {
-        return Collections.singleton(
-                new ReadArcQueryExecutable((ViewPlaceInstance) placeInstance, tokenVar, stateRecorder)
-        );
+        return Collections.emptySet();
     }
 }
